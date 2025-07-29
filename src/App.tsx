8e6b74982e75773d70bbee939dac04d0bfec9bb2
Sahil <issahil404@gmail.com>
2025-07-29 23:45:06 +0530

    added test themeing component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
 import { MainLayout } from "./components/Layout";
+import { ThemeProvider } from "./lib/theme-context";
+import { ThemeTest } from "./components/ThemeTest";
 
 function App() {
-  return <MainLayout />;
+  return (
+    <ThemeProvider>
+      <div className="min-h-screen bg-gray-50 dark:bg-gray-900 transition-colors duration-200">
+        <ThemeTest />
+        <MainLayout />
+      </div>
+    </ThemeProvider>
+  );
 }
 
 export default App;
